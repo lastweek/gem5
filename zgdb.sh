470880d51b7593ae8e90af7d9e0b762144560d97
Yizhou Shan <shan13@purdue.edu>
2016-12-08 12:40:35 -0500

    asd

--- zgdb.sh
+++ zgdb.sh
@@ -2,7 +2,7 @@
 
 BENCH=sjeng
 OUTPUT_DIR=tests/test1/${BENCH}
-NR_MAXINSTS=1000000
+NR_MAXINSTS=2000
 TLB_MEMORY_LATENCY=30ns
 #DEBUG_FLAG=--debug-flag=LSQUnit,Cache,TLB,CoherentBus,Fetch
 
@@ -12,8 +12,8 @@ CPU_MODE=--cpu-type=inorder
 gdb --args \
 	./build/ALPHA_MESI_CMP_directory/gem5.opt		\
 		${DEBUG_FLAG} -d ${OUTPUT_DIR}			\
-		configs/spec2k6_classic/run.py			\
-			${CPU_MODE} --caches			\
-			-b ${BENCH}				\
+		configs/spec2k6_classic/se.py \
+			--bench ${BENCH}				\
 			--maxinsts=${NR_MAXINSTS}		\
-			-l ${TLB_MEMORY_LATENCY}
+			--checkpoint-at-end \
+			--take-checkpoint=100 --at-instruction -S
